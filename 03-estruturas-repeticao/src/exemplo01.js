f27cad195a5963a77427b5488829dbc266cde93d
Tamireszim <tamires_zim@estudante.sesisenai.org.br>
2024-06-19 21:37:30 -0300

    Estruturas de repetição

---
+++ 03-estruturas-repeticao/src/exemplo01.js
@@ -0,0 +1,26 @@
+//while(condition){
+//
+//}
+//------------------------------------
+//do{
+//
+//}while (condition) {
+//    
+//}
+//-----------------------------------------
+//for (let index = 0; index < array.length; index++) {
+//    const element = array[index];
+//    
+//}
+var estaCarregando = true;
+var porcentagem = 0;
+
+while (estaCarregando) {
+    console.log(porcentagem + "% CARREGANDO...");
+    
+if(porcentagem < 100){
+    porcentagem += 5;
+}else {
+    estaCarregando = false
+}
+}
